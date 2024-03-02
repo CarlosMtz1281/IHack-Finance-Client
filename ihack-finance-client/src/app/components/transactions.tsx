0e39f428d660e09f97905dd43ffc7a56b981e3e7
Carlos Martinez <carlosh.mtz@hotmail.com>
2024-03-02 12:04:46 -0600

    dashbiard layout basic

--- ihack-finance-client/src/app/components/transactions.tsx
+++ ihack-finance-client/src/app/components/transactions.tsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React from "react";
+
+import "../style/components/transactions.css";
 
 interface TransactionProps {
-    name: string;
-    amount: number;
-    category: string;
-    date: Date;
+  name: string;
+  amount: number;
+  category: string;
+  date: Date;
 }
 
-const Transaction: React.FC<TransactionProps> = ({ name, amount, category, date }) => {
-    return (
-        <div className="transaction-box">
-            <div className="transaction-name">{name}</div>
-            <div className="transaction-amount">${amount}</div>
-            <div className="transaction-category">{category}</div>
-            <div className="transaction-date">{date.toDateString()}</div>
-        </div>
-    );
+const Transaction: React.FC<TransactionProps> = ({
+  name,
+  amount,
+  category,
+  date,
+}) => {
+  return (
+    <div className="transaction-box">
+    <div className={`transaction-category ${category === '1' ? 'red' : category === '2' ? 'yellow' : category === '3' ? 'blue' : ''}`}>
+
+    </div>
+      <div className="headerLeft">
+        <div className="transaction-name">{name}</div>
+        <div className="transaction-date">{date.toDateString()}</div>
+      </div>
+      <div className="transaction-amount">${amount}</div>
+    </div>
+  );
 };
 
 export default Transaction;
--- ihack-finance-client/src/app/dashboard/page.tsx
+++ ihack-finance-client/src/app/dashboard/page.tsx
@@ -6,34 +6,33 @@ import "../style/dashboard.css";
 import Transaction from "../components/transactions";
 
 const transactions = [
-    {
-        amount: 1000,
-        cost: 10,
-        name: "Transaction 1",
-        category: "Category 1",
-        date: new Date(),
-    },
-    {
-        amount: 2000,
-        cost: 20,
-        name: "Transaction 2",
-        category: "Category 2",
-        date: new Date(),
-    },
-    {
-        amount: 3000,
-        cost: 30,
-        name: "Transaction 3",
-        category: "Category 3",
-        date: new Date(),
-    },
+  {
+    amount: 1000,
+    cost: 10,
+    name: "Transaction 1",
+    category: "1",
+    date: new Date(),
+  },
+  {
+    amount: 2000,
+    cost: 20,
+    name: "Transaction 2",
+    category: "2",
+    date: new Date(),
+  },
+  {
+    amount: 3000,
+    cost: 30,
+    name: "Transaction 3",
+    category: "3",
+    date: new Date(),
+  },
 ];
-
 
 const DashboardPage: React.FC = () => {
   return (
     <div className="dashboardWrap">
-      <h1>Bienvenido Carlos</h1>
+      <h1 className="dashboardMainTittle">Bienvenido Carlos</h1>
       {/* Add your dashboard components and content here */}
       <div className="graphSection">
         <PieChart
@@ -54,14 +53,20 @@ const DashboardPage: React.FC = () => {
         />
       </div>
       <div className="expensesSection">
-          <h2>Ultimas Transacciones</h2>
+        <h2 className="expenssesTittle">Ultimas Transacciones</h2>
 
-            <div className="transactionContainer">
-                {/*COMPONENT */}
-                <Transaction name="Comida" amount={1000} category="Comida" date={new Date()} />
-
-            </div>
-
+        <div className="transactionContainer">
+          {/* COMPONENT */}
+          {transactions.map((transaction, index) => (
+            <Transaction
+              key={index}
+              name={transaction.name}
+              amount={transaction.amount}
+              category={transaction.category}
+              date={transaction.date}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
